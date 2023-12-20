fad2e8af1874eee8bdb3d43345a574fe751700e5
Chintanneosoft <chintan.rajgor@neosoftmail.com>
2023-12-20 12:06:14 +0530

    Base Project Initial

--- DemoLoader/ViewController.swift
+++ DemoLoader/ViewController.swift
@@ -6,14 +6,23 @@
 //
 
 import UIKit
+import NVActivityIndicatorView
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let x = self.view.center.x
+        let y = self.view.center.y
+
+        let frame = CGRect(x: (x - 50), y: (y - 50), width: 100, height: 100)
+        let activityIndicatorView = NVActivityIndicatorView(frame: frame)
+
+        activityIndicatorView.type = .ballClipRotateMultiple
+        activityIndicatorView.color = UIColor.blue
+
+        self.view.addSubview(activityIndicatorView)
+        activityIndicatorView.startAnimating()
     }
-
-
 }
 
